62d4e4644993b509de5452e0c332c69753d58200
aguessous <aguessous@gmail.com>
2019-12-04 20:49:37 -0500

    local 04

--- __main__.py
+++ __main__.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     # partie = PartiePipopipette()
 
     # Pour charger d'une partie déjà sauvegardée
-    partie = PartiePipopipette("interface/partie_en_cours.txt")
+    # partie = PartiePipopipette("partie_sauvegardee.txt")
 
     # Pour sauvegarder une partie
     # partie.sauvegarder("partie_sauvegardee.txt")
 
     # partie.jouer()
-    f = Fenetre(partie)
+    f = Fenetre()
     f.mainloop()
